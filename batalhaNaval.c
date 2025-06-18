e97921832e7227a684f688887bc7fe484716e534
Fabio <fabfloriano@hotmail.com>
2025-06-18 02:59:13 +0000

    finalizado nivel aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,92 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define TAMANHO_TABULEIRO 10
+#define TAMANHO_NAVIO 3
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO] = {0};
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    int navioH[TAMANHO_NAVIO] = {3, 3, 3};
+    int navioV[TAMANHO_NAVIO] = {3, 3, 3};
+    int navioD1[TAMANHO_NAVIO] = {3, 3, 3}; 
+    int navioD2[TAMANHO_NAVIO] = {3, 3, 3};
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    int linhaH = 2, colunaH = 1; 
+    int linhaV = 5, colunaV = 4;  
+    int linhaD1 = 0, colunaD1 = 0;     
+    int linhaD2 = 0, colunaD2 = 9;      
+
+
+    if (colunaH + TAMANHO_NAVIO <= TAMANHO_TABULEIRO) {
+        int podePosicionar = 1;
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            if (tabuleiro[linhaH][colunaH + i] != 0) {
+                podePosicionar = 0;
+                break;
+            }
+        }
+        if (podePosicionar) {
+            for (int i = 0; i < TAMANHO_NAVIO; i++) {
+                tabuleiro[linhaH][colunaH + i] = navioH[i];
+            }
+        }
+    }
+
+    if (linhaV + TAMANHO_NAVIO <= TAMANHO_TABULEIRO) {
+        int podePosicionar = 1;
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            if (tabuleiro[linhaV + i][colunaV] != 0) {
+                podePosicionar = 0;
+                break;
+            }
+        }
+        if (podePosicionar) {
+            for (int i = 0; i < TAMANHO_NAVIO; i++) {
+                tabuleiro[linhaV + i][colunaV] = navioV[i];
+            }
+        }
+    }
+
+
+    if (linhaD1 + TAMANHO_NAVIO <= TAMANHO_TABULEIRO && colunaD1 + TAMANHO_NAVIO <= TAMANHO_TABULEIRO) {
+        int podePosicionar = 1;
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            if (tabuleiro[linhaD1 + i][colunaD1 + i] != 0) {
+                podePosicionar = 0;
+                break;
+            }
+        }
+        if (podePosicionar) {
+            for (int i = 0; i < TAMANHO_NAVIO; i++) {
+                tabuleiro[linhaD1 + i][colunaD1 + i] = navioD1[i];
+            }
+        }
+    }
+
+    if (linhaD2 + TAMANHO_NAVIO <= TAMANHO_TABULEIRO && colunaD2 - (TAMANHO_NAVIO - 1) >= 0) {
+        int podePosicionar = 1;
+        for (int i = 0; i < TAMANHO_NAVIO; i++) {
+            if (tabuleiro[linhaD2 + i][colunaD2 - i] != 0) {
+                podePosicionar = 0;
+                break;
+            }
+        }
+        if (podePosicionar) {
+            for (int i = 0; i < TAMANHO_NAVIO; i++) {
+                tabuleiro[linhaD2 + i][colunaD2 - i] = navioD2[i];
+            }
+        }
+    }
+
+    printf("\nTabuleiro de Batalha Naval:\n\n");
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
